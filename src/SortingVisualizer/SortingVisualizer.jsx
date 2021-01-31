a87bac5b7170710f0b434b84b7471a8cdb13498f
PratikshaSh <pratiksha8520@gmail.com>
2021-01-31 22:33:44 +0530

    create bars

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -16,21 +16,24 @@ componentDidMount(){
 
 resetArray(){
     const array = [];
-    for(let i=0;i<100;i++){
-        array.push(randomIntFromInterval(5,1000));
+    for(let i=0;i<270;i++){
+        array.push(randomIntFromInterval(5,550));
     }
     this.setState({array});
 }
  render(){
   const {array} = this.state;
-  return(
-      <>
-      {array.map((value, idx)=> (
-          <div className="array-bar" key= {idx}>
-          {value}
-          </div>
-      )) }
-      </>
+  return (
+    <div className="array-container">
+      {array.map((value, idx) => (
+        <div
+          className="array-bar"
+          key={idx}
+          style={{
+            height: `${value}px`
+          }}></div>
+      ))}
+      </div>
   );
   }
  
