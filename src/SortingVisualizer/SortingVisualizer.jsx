1644197f12ddc1eb3122e9ca0053d605e0ccb84a
PratikshaSh <pratiksha8520@gmail.com>
2021-02-02 01:44:52 +0530

    test button added

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -34,7 +34,19 @@ heapSort() {}
 
 bubbleSort() {}
 
-
+testSortingAlgorithms(){
+ for(let i=0;i<100;i++){
+   const array = [];
+   const length = randomIntFromInterval(1,1000);
+   for(let i=0;i<length;i++){
+     array.push(randomIntFromInterval(-1000,1000));
+   }
+ 
+  const javaScriptSortedArray = array.slice().sort((a,b)=> a-b);
+  const mergeSortedArray = SortingAlgorithms.mergeSort(array.slice());
+  console.log(arraysAreEqual(javaScriptSortedArray,mergeSortedArray));
+ }
+}
 
  render(){
   const {array} = this.state;
@@ -53,6 +65,7 @@ bubbleSort() {}
         <button onClick={() => this.quickSort()}>Quick Sort</button>
         <button onClick={() => this.heapSort()}>Heap Sort</button>
         <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+        <button onClick={() => this.testSortingAlgorithms()}>Test Sorting Algorithms</button>
       </div>
   );
   }
